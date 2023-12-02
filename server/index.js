2fb382681d4e5b00f4fb26ff052eff032842b680
Abloniyem <abelsolomon2821@gmail.com>
2023-12-02 05:23:20 -0800

    upadate

--- server/index.js
+++ server/index.js
@@ -44,6 +44,6 @@ app.use((err, req, res, next) => {
 });
 app.listen(process.env.PORT || "4000", () => {
   connect();
-  console.log("server connected....");
+  console.log("server connected.....");
 
 });
