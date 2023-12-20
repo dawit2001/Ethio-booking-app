755ed79ef59fb60b7bd17ce2f5da97d697fc9e1d
dawit2001 <abebedawit2001@gmail.com>
2023-12-20 16:32:00 +0300

    modified

--- client/src/components/Header.jsx
+++ client/src/components/Header.jsx
@@ -9,7 +9,7 @@ import axios from "axios";
 import ProfilePopOver from "./ProfilePopOver";
 
 const Header = () => {
-  console.log(import.meta.env.MODE);
+  console.log(import.meta.env.VITE_API_URL);
   const location = useLocation();
   const [user, setUser] = useState(null);
   const checkRoutes = () => {
